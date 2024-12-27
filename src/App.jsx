028afad4d0e75394d437447341fccb976da80c14
poongkamali <159992437+poongkamalis@users.noreply.github.com>
2024-12-27 10:36:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getadvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(function () {
+    getadvice();
+  }, []);
+  return (
+    <>
+      <h1>{advice}</h1>
+      <button onClick={getadvice}>get advice</button>
+      <Message count={count} />
+    </>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      you have read <strong>{props.count}</strong> pices of advice
+    </p>
+  );
+}
